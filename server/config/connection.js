6dc90f54abea3bc307f60c3d56d1cd970bc5bf13
Cody Thompson <codylthomp@gmail.com>
2024-02-06 22:16:06 -0600

    Mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-thoughts'
-);
+const MONGODB_URI = 'mongodb+srv://codylthomp:xxtrU4usqx5XqPtC@gameshub.m9dymoh.mongodb.net/Gameshub';
+
+mongoose.connect(MONGODB_URI, {
+ 
+ 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB Atlas - Gameshub');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+  
 
 module.exports = mongoose.connection;
