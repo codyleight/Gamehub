b63dbfa22d064e29f3b884deb6297213ef18e679
Cody Thompson <codylthomp@gmail.com>
2024-02-06 22:45:03 -0600

    Vite and package update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +8,19 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+
+
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
         secure: false,
-      },
-    },
+        changeOrigin: true
+      }
+    }
+
   },
-});
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
+})
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+//todo: REINTRODUCE BCRYPT
 
+const { Schema } = mongoose;
+//const bcrypt = require('bcrypt');
 const Order = require('./Order');
 
 const userSchema = new Schema({
@@ -31,7 +33,8 @@ const userSchema = new Schema({
 // set up pre-save middleware to create password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
-   
+    //const saltRounds = 10;
+   // this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
   next();
@@ -39,7 +42,7 @@ userSchema.pre('save', async function (next) {
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-  
+  // return await bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
