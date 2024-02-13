cd26b796553c605af9878cb7e183c58c38a9167a
Jaysahhn <jasonmason96@yahoo.com>
2024-02-13 17:54:54 -0600

    code readability

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,23 +1,18 @@
 import { Link } from 'react-router-dom';
 
-
-
 export default function Footer() {
-  
-
-
   return (
-<div className='footer text-center text-white'>
-    Created By: {" "}
-    <Link key={1} className="text-warning" to="https://github.com/codyleight">
-            Cody Thompson 
-          </Link> {" "}
-          <Link key={1} className="text-primary" to="https://github.com/bolfati">
-            Bijan Olfati
-          </Link> {" "}
-          <Link key={1} className="text-danger" to="https://github.com/Jaysahhn">
-            Jason Mason
-          </Link>
-</div>
+    <div className='footer text-center text-white'>
+      Created By: {" "}
+      <Link key={1} className="text-warning" to="https://github.com/codyleight">
+        Cody Thompson
+      </Link> {" "}
+      <Link key={1} className="text-primary" to="https://github.com/bolfati">
+        Bijan Olfati
+      </Link> {" "}
+      <Link key={1} className="text-danger" to="https://github.com/Jaysahhn">
+        Jason Mason
+      </Link>
+    </div>
   );
 }
